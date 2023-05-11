5e8d7ce874fa67dc53944e8f6dbca736d3adf007
Anton <ForWorkCodes@gmail.com>
2023-05-11 19:10:43 +0300

    Подключение vue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'
+
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js', // Используйте ваш основной файл JavaScript
+            output: 'public/js/app.js', // Укажите путь и имя для выходного файла
             refresh: true,
         }),
     ],
-});
+    build: {
+        manifest: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].js', // Указываем формат имени файла
+                chunkFileNames: 'assets/[name].js', // Указываем формат имени файла
+                assetFileNames: 'assets/[name].[ext]', // Указываем формат имени файла
+            },
+        },
+    },
+})
